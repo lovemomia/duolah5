6ff2bb53ba3919f67a2cc63322b9d58cf265f3c8
ysm9099 <909912314@qq.com>
2015-08-07 17:22:41 +0800

    下载app页面

---
+++ src/main/java/cn/momia/duolah5/web/controller/DownloadAppController.java
@@ -0,0 +1,17 @@
+package cn.momia.duolah5.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+/**
+ * Created by ysm on 15-8-7.
+ */
+@Controller
+public class DownloadAppController extends BaseFunc {
+    @RequestMapping(value = "/downapp.html")
+    public ModelAndView downloadApp() {
+        return new ModelAndView("/downloadApp");
+    }
+}
