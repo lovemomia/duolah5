85d9a8996fff162435dff99ed6d908439bd34a86
吴秋锋 <arthas.wuqf@gmail.com>
2015-08-11 17:42:13 +0800

    调整内存

--- bin/env.sh
+++ bin/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-JAVA_OPTS="-Xms512M -Xmx1G -Xmn128M \
-	-XX:PermSize=32M -XX:MaxPermSize=32M \
+JAVA_OPTS="-Xms512M -Xmx1G -Xmn192M \
+	-XX:PermSize=64M -XX:MaxPermSize=64M \
 	-XX:+UseConcMarkSweepGC -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=80 \
 	-XX:+PrintGCDetails -XX:+PrintGCDateStamps -verbose:gc -XX:+DisableExplicitGC \
 	-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=6666"
