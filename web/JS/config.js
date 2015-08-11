9e26ddfd473245b4a07d6b91da0367eecb890c02
ysm9099 <909912314@qq.com>
2015-08-11 18:32:02 +0800

    修复sku bug

--- web/JS/config.js
+++ web/JS/config.js
@@ -4,5 +4,6 @@ var tq = {
   url: "http://i.momia.cn/m/",
   urls: "https://i.momia.cn/m/",
   R: 6378.137,
-  appid: "wxc4ca5800d28b5650"
+  appid: "wxc4ca5800d28b5650",
+  uploadurl:'http://upload.momia.cn/upload/image'
 };
