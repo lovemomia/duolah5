7da96d0c32b6207cefed249b243afdb3a5c69acf
吴秋锋 <arthas.wuqf@gmail.com>
2015-08-28 15:50:12 +0800

    删除订单

--- web/JS/config_v1.js
+++ web/JS/config_v1.js
@@ -1,8 +1,8 @@
 var tq = {
   doing: false,
   p: {}, // 当前页
-  url: "http://i.momia.cn/m/",
-  urls: "https://i.momia.cn/m/",
+  url: "http://localhost:8080/m/",
+  urls: "http://localhost:8080/m/",
   R: 6378.137,
   appid: "wxc4ca5800d28b5650",
   uploadurl:'http://upload.momia.cn/upload/image'
